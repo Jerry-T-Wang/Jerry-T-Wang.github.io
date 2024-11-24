e4a891743d86b7c55da845aeb4b42bc2e3a8ab9a
Jerry-T-Wang <1192283904@qq.com>
2024-11-24 22:24:37 +0800

    Site updated: 2024-11-24 22:24:37

--- myblog/js/custome.js
+++ myblog/js/custome.js
@@ -1,13 +1,1 @@
-var OriginTitle = document.title;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        document.title = '╭(°A°`)╮ 你去哪了? 快回来!!!';
-    }else {
-        document.title = '(ฅ>ω<*ฅ) 你终于回来了 ~';
-        setTimeout(function () {
-            if(!document.hidden){
-                document.title = OriginTitle;
-            }
-        }, 2000);
-    }
-});
+var OriginTitle=document.title,darklighttoggle=(document.addEventListener("visibilitychange",function(){darklighttoggle(),document.hidden?document.title="╭(°A°`)╮ 你去哪了? 快回来!!!":(document.title="(ฅ>ω<*ฅ) 你终于回来了 ~",setTimeout(function(){document.hidden||(document.title=OriginTitle)},2e3))}),function(){var t=volantis.dark.mode;console.log(t)});
